d3d5b64305c809351ff0f0d32661a92637b5ca57
Yerdaulet <yerdauletacc@gmail.com>
2026-05-13 11:15:50 +0500

    initial commit

---
+++ src/main/java/com/example/fast_cv/FastCvApplication.java
@@ -0,0 +1,13 @@
+package com.example.fast_cv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastCvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastCvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fast_cv/FastCvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fast_cv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastCvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
